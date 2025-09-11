8a411fcffe8428bfbb31cf01391820e60ebb53e0
namemc95 <moralesdreiser5@gmail.com>
2025-09-11 11:20:25 -0500

    week 2 coments added in program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,12 @@
+// For this program I have save all the qeustions in a json file
+// instead of having the questions in any other part of the code
+// i have separated the pronts by topict and from the json file
+// we first have displayed the topic and the user can chose what
+// topic they want to write about, and the depending in the topic 
+// a random question is displayed
+// the program also has the ability to save the information in a
+// csv file which can be open in excel.
+
 class Program
 {
     static void Main(string[] args)
