e58dd0648a15b96f5c548f81d50b8b20e342fe9b
namemc95 <moralesdreiser5@gmail.com>
2025-09-18 12:04:07 -0500

    comments added

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,5 +1,11 @@
-using System;
-using System.Threading.Tasks;
+// for this program i'm retriving information form a webside  
+// i just get random verses and then used the clases to organize it and  
+// display on the screem hidding words randomly 
+// i also hacve the option of memorizin ine verse or 2 verses
+// the program was growing to much so i did not add mote features 
+// it contains the 3 clasess and some more i used to handled errors 
+// i also have a menu with the 2 options to memorize a simple verse 
+// or multople verse both retrived form internet 
 
 class Program
 {
