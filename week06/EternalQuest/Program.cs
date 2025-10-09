789b7afd14e60ed25fdb94a2f66b3ab660a9435c
namemc95 <moralesdreiser5@gmail.com>
2025-10-09 16:59:15 -0500

    comments updated

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,8 +1,8 @@
 using System;
 
 /*
-i added a _CreatedAt atribute in goals calss to keep record of the lengh of completion for some goals when needed
 i added a _completedTimes atribute in eternal goals to add a bonus each 5 times of completing a goal in the logic of record events 
+i implemented the sleep method to show messages when lopading files and when saving files 
 */
 
 class Program
